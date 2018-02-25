e14a7c9448454437974fa11a510e1c65357437cf
romulofssGit <fssromulo@gmail.com>
2018-02-25 18:23:31 -0300

    Teste push notification

--- js/app.js
+++ js/app.js
@@ -13,6 +13,21 @@ if ('serviceWorker' in navigator) {
 const publicKey = 'BHR_KlKwm1SQd9qCCxIlxHz86Qch70V4kszQVOCKTCJkFh2gRSmp33J7hqSwHuLgDC7a8gaWMIN-rhSQyyTcTSc';
 // = 'Aca8mYiAJgDmKyt-OR3M1D7UqY4p8dhvH_ps8oIgIrE';
 
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 navigator.serviceWorker && navigator.serviceWorker.ready.then(
 	function(serviceWorkerRegistration) {  
   		serviceWorkerRegistration.pushManager.getSubscription()  
